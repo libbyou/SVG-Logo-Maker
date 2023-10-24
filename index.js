b31b6ac5d42a7fd180232c88905303e400b224a8
libbyou <lcyou95@gmail.com>
2023-10-24 14:11:00 -0500

    added text and text color for shapes.js and shapes.test.js. also creted the cli.js and added the questions within it

---
+++ index.js
@@ -0,0 +1,10 @@
+const CLI = require('./lib/cli.js');
+
+const cli = new CLI();
+
+cli.run();
+
+//run tests for logo outcomes
+//when ran, questions about desired logo is asked
+//responses taken to make logo
+//logo with all desired characteristics rendered
--- testing/shapes.test.js
+++ testing/shapes.test.js
@@ -3,9 +3,12 @@ const {Triangle, Circle, Square} = require ('../lib/shapes');
 // test for triangle class
 describe ('Shapes', () => {
     describe ('blue', () => {
-        it ('should render a blue triangle when given the shape and color', () => {
-            const triangle = new Triangle("blue");
-            expect(triangle.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue"/>');
+        it ('should render a blue triangle with colored text when given the shape, colors, and text specifics', () => {
+            const triangle = new Triangle("blue", "Hello!", "white");
+            // triangle.setColor("blue");
+            // triangle.setText("Hello!");
+            // triangle.setTextColor("white");
+            expect(triangle.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue"><text x="10" y="10" fill="white">Hello!</text></polygon>');
         });
     });
 });
@@ -13,9 +16,12 @@ describe ('Shapes', () => {
 // test for circle class
 describe ('Shapes', () => {
     describe ('green', () => {
-        it ('should render a green circle when given the shape and color', () => {
-            const circle = new Circle ("green");
-            expect(circle.render()).toEqual('<circle cx="25" cy="75" r="20" fill="green"/>');
+        it ('should render a green circle with colored text when given the shape, color, and text specifics', () => {
+            const circle = new Circle ("green", "I love coding!", "pink");
+            // circle.setColor("green");
+            // circle.setText("I love coding!");
+            // circle.setTextColor("pink");
+            expect(circle.render()).toEqual('<circle cx="25" cy="75" r="20" fill="green"><text x="10" y="10" fill="pink">I love coding!</text></circle>');
         });
     });
 });
@@ -23,9 +29,12 @@ describe ('Shapes', () => {
 // test for square class
 describe ('Shapes', () => {
     describe ('yellow', () => {
-        it ('should render a yellow square when given the shape and color', () => {
-            const square = new Square("yellow");
-            expect(square.render()).toEqual('<rect x="10" y="10" width="30" height="30" fill="yellow"/>');
+        it ('should render a yellow square with colored text when given the shape, color, and text specifics', () => {
+            const square = new Square("yellow", "Have a great day!", "black");
+            // square.setColor("yellow");
+            // square.setText("Have a great day!");
+            // square.setTextColor("black");
+            expect(square.render()).toEqual('<rect x="10" y="10" width="30" height="30" fill="yellow"><text x="10" y="10" fill="black">Have a great day!</text></rect>');
         });
     });
 });
